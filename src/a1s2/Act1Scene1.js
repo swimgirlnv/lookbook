09bf47b78f14cc98e1898f6075ec6ebb94a71fb7
Becca <rebecca_waterson@brown.edu>
2023-11-20 17:09:10 -0500

    title

---
+++ src/a1s2/Act1Scene1.js
@@ -0,0 +1,12 @@
+import "./Act1Scene1.css";
+
+function Act1Scene1() {
+  
+  return (
+    <div className="Act1Scene1">
+      
+    </div>
+  )
+}
+
+export default Act1Scene1;
---
+++ src/topbar/TopBar.js
@@ -0,0 +1,15 @@
+import "./TopBar.css";
+
+function TopBar() {
+  
+  return (
+    <div className="TopBar">
+      <div className="top-bar" id="navbar">
+        <a className="navbar-first" href="#Top"><strong>Phantom of the Disco</strong></a>
+      </div>
+    </div>
+  )
+}
+
+export default TopBar;
+
