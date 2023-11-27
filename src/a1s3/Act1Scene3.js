3109f0273e4acbe4cfd37ded296323dddce399e8
Becca <rebecca_waterson@brown.edu>
2023-11-27 18:27:06 -0500

    fixed images

--- src/a1s3/Act1Scene3.js
+++ src/a1s3/Act1Scene3.js
@@ -39,11 +39,11 @@ function Act1Scene3() {
         <div className="row-imgs">
           <div className="">
             <img style={{height:"260px"}}
-            src="https://lh3.googleusercontent.com/pw/ADCreHdVWIojldC6i8lP-WkJ_57dtZyRhrLBML7NCShKEsVXpHdpqBhul9tX2ysDQUFNQy2vPAMHSWj8Yeeiypm_5sId8lnKXMLweoxbmi_usnRCDf4jJKtjg0A692DV_xpBdfU_yS85D9dPg3EmNDWMgTlnlx83FVERH3fZqjztYwTtw7eZ52zfKnrhq43Qq1k-oabsahghZInsyLn5d3VmLpn5eDFrVuO2xiJ6NSubb2Dj-dkKfLJrsyDtWnDJzniZXHgKJI0Hc96y4w6Gd6ybfY7ZMajhNR82fHg1iRx3FzC3ym19NFm7oYE5n-evmeZsaoa7ugty8GRPqCjOUu60EGOhv2PeNGn_9kfeTxqFnU_JbK0p-e7q3XgN1unFdB_9aUmiLN2q9kOhBlGDMwEYnyA79fzyH-Un0JVWtq6ogJVGmWGU8db8dky3UqHkyulli7ORS_NzmbmApP1snFljkKOFWZSk3IFIIBJvNfnk4_mZx6fyDulryEKiFrTOv2r8Ik9C_3BqSeUj5gHisStIzbJ6rsMjfRA77D8ojMhNRWkLRtRhnFYdYjRHlXvlbuLkJHQujytHmu-5olina0QCkFUWbJbqjCL90w48QEVoYPhzesdBtk7iU8054J3Rp_nfE1zyy3OUqIjmiEJakrpmuEZWEmdjy_ojLOPg7rpD0oh5DQsRC0oqxrFUReMO9WvIp70uOV184ERWA7MfRiy801OIB8YPWAU3PLtwiFmHH2rfZW-7N_MDVv_JkJMdcSom6p3AdfWikBInYNj1uG2ELGU8-5AOqFJ8DWY4KLdg3m5DaGJBnfc-kOHP7Xi3GeIF7Icl6Bc6CVu-CgHrNBjEmiArQQNMY0B-NcHFlOgBWlHagllSnAkCDL5MBSuIPLx1xv6KnxBmaxIs_yx4D-2iu0b6bgQ2z5aG6u9rb9hTTBNN3-qaULQkJ7P7cKLvgflbc6c0-oUevNLTson255l5TDiZpAnUq7JNvFQGS6DUnof-tJXD=w2662-h1996-s-no-gm?authuser=1" alt="Kylie"></img>
+            src="https://i.imgur.com/xxetPIF.jpg" alt="Kylie"></img>
           </div>
           <div className="">
             <img style={{height:"260px"}}
-            src="https://lh3.googleusercontent.com/pw/ADCreHdSWpmUuxwMjXQl41Ow8LUbAw3RSOe2Y7On-8TjLdLoZpygciJudf3nJ7LEvKBdqcSeRnoiY-S-Z_tTz_a2H8uid79rf1fIq-l5K4Ut0_0Tf6ioQNKpxEeRsLCWo08s3uXinvGYkIIpYixngGxiHXYbwT-v97N0GE15h58R7QIUZVIghy4PYa7qtsmppVspgINriKGPifogOrOccFGtDr7NTPfxREXm3Q7j8WBEhQdf5rqRLzTXdODGUqCYspSOuvDt-uVLbxI9FtzxPRCHNtb2J5ucLcQIoN0SKVrJ3UE1uF5nX_2mu5JCYgUl_vobR1CDZKsx4xa2Bnz3NdAW5CqlrsVsELEeH36jqgw8ggH6bVthxMPaocELfV-hkdCTfWofXkyV9RDR3e4fLkBN1oqq0Ii61AM-Xwtm6V4XN2zhaaVNvf-Zci_SUd16ElESbiybnVkNzBD1dS9Kh3658EYJ-oFZdrCXjhngV56Wd11ebdf44IyyFiFZY0fkKdSfj76R4wwEIoIkW255W04-a_Tl7g-8OJvTI5F1dm_oZAh0T7EwrjbcvxoPcdGhTZ_TVC8lbkuXYOacj4mcl0lwclyZJRE-KItU0Co6fliohqh02SqgXnpzXIHCWnzYwivVxbt041c7kGe0CR_PJcKzf_27f-nomlIa1SJZ7iHAPpyMOk1_ki1QgGyJPEzl_5e7clJ3QLoHYQV3q56LrTgn40uCHzQ5kPOVusEdPzNK5NEJ-dVmF-HmeryIrBclIW6psHEaGRYkDRVinEK77TL89s2K3tPGojn1mt-kbL9ZksXTObWzLhbnHH-HBMynVBWatmvvEIaXGVr96vCzX-37ralbRT1AbUmlL1dIbTQApKurYxgf_hbNDOMr0LnjyWLbSGZmxYFPiOUB6_GfBRCG_TrRCD92U-mPihYNMysUsX5elcQCitN5-nyCc1hJmrSXv1Ssy1vjbfQkethh9euRkDNlYi_ekrBZMnNZ5i_I4QaU5GeG=w2662-h1996-s-no-gm?authuser=1" alt="Kylie"></img>
+            src="https://i.imgur.com/6HqluWu.jpg" alt="Kylie"></img>
           </div>
         </div>
       </div>
